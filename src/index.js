7a64cac51613b065a641365818f720c86097d270
ibattistax <isabel.battista@gmail.com>
2024-06-01 00:02:33 +0100

    Added API integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-function generateCompliment(event) {
-  event.preventDefault();
-
+function displayCompliment(response) {
+  console.log("Poem generated");
   new Typewriter("#compliment", {
-    strings: "The compliment will go here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generateCompliment(event) {
+  event.preventDefault();
+  console.log("generating poem");
+
+  let userInformation = document.querySelector("#user-information");
+
+  let apiKey = "oaff0b06238ce15e1bda3c4dt5f4a7ba";
+  let context =
+    "You are a curmudgeonly, grumpy person who doesn't like people nor giving compliments, but secretly finds beauty in them. Please limit your response to two lines, and please sound conversational.";
+  let prompt = `Please give a compliment to a person on the topic of the ${userInformation} as though you are responding to them and seeing them on the street. They are a human being.`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayCompliment);
 }
 
 let formElement = document.querySelector("#compliment-generator-form");
