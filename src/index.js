d8732952c00dfac98874fd405644c93aa0529082
ibattistax <isabel.battista@gmail.com>
2024-06-02 16:02:12 +0100

    Finishing touches in CSS and JS

--- src/index.js
+++ src/index.js
@@ -14,13 +14,13 @@ function generateCompliment(event) {
 
   let apiKey = "oaff0b06238ce15e1bda3c4dt5f4a7ba";
   let context =
-    "You are a curmudgeonly AI Assistant who doesn't like people but secretly finds beauty in them. You are uncomfortable giving compliments and tend to use sarcasm. Please ensure your response is detailed and 2-4 lines long. Please sound conversational. Please don't indicate in any way that you are an AI, different from a human. Please make sure the sentence makes sense.";
+    "You are a curmudgeonly AI Assistant who doesn't like people but secretly finds beauty in them. You are uncomfortable giving compliments and tend to use sarcasm. Please ensure your response is detailed and 2-3 lines long. Please sound conversational. Please don't indicate in any way that you are an AI, different from a human. Please make sure the sentence makes sense.";
   let prompt = `Please give a compliment to a person on the topic of the ${userInformation.value} as though you are another person complimenting them on something they've just shown you. They are a human being.`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let complimentElement = document.querySelector("#compliment");
   complimentElement.classList.remove("hidden");
-  complimentElement.innerHTML = `<div class = "generating"> ⏳ Getting ready to compliment you on your ${userInformation.value} skills</div>`;
+  complimentElement.innerHTML = `<div class = "generating"> ⏳ Getting ready to compliment you...</div>`;
 
   axios.get(apiUrl).then(displayCompliment);
 }
